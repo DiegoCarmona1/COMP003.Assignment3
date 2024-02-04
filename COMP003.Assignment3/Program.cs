5659cb3ae4f1122cde19f9f01b089ae8e63fb9d3
Diego Carmona <diegocarmona@my.whccd.edu>
2024-02-03 16:55:12 -0800

    Completed switch section, completed project

--- COMP003.Assignment3/Program.cs
+++ COMP003.Assignment3/Program.cs
@@ -27,9 +27,37 @@ namespace COMP003.Assignment3
             else if (numericGrade >= 60) { Console.WriteLine("Letter Grade: D"); }
             else { Console.WriteLine("Letter Grade: F"); } 
             if (numericGrade == double.NaN) { Console.WriteLine("Invalid Input"); }
-            
-            //Switch Section
 
+            //Switch Section
+            Console.WriteLine("Input Day of week: (1-7)");
+            string inputDay = Console.ReadLine();
+            int numericDay = Convert.ToInt32(inputDay);
+            switch (numericDay)
+            {
+                case 1:
+                    Console.WriteLine("String Day: Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("String Day: Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("String Day: Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("String Day: Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("String Day: Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("String Day: Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Strinmg Day: Sunday");
+                    break;
+                default: Console.WriteLine("Invalid Day");
+                    break;
+            }
         }
     }
 }
